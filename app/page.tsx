09de49f52b5578de90828a28304497f19f2cf709
moses mwangi <moses.mwangi.me@gmail.com>
2024-07-02 10:10:31 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-blue-400">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sint
+      adipisci dolorum nemo quo explicabo nostrum fugit nulla, eum suscipit
+      sapiente quas asperiores dolores eaque, amet nihil blanditiis numquam
+      praesentium?
+    </main>
+  );
+}
